6a676d8bf085fc2b81c1242ffc2a95ec759b1eac
djawnstj <90193598+djawnstj@users.noreply.github.com>
2023-01-24 17:22:16 +0900

    [Silver V] Title: 수들의 합 5, Time: 176 ms, Memory: 13272 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.kt
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.kt
@@ -5,20 +5,35 @@ fun main() {
 
     val num = sc.nextInt()
 
+    var lt = 1
+    var rt = 1
+
+    var sum = 0
     var result = 0
 
-    for (i in 1 .. num) {
-        val lt = i
-        var temp = 0
-        for (j in i .. num) {
-            temp += j
-            if (temp > num) break
-            else if (temp == num) {
-                result++
-                break
-            }
+    while (lt <= num) {
+        sum += rt
+        if (sum >= num) {
+            if (sum == num) result++
+            lt++
+            sum = 0
+            rt = lt
         }
+        else rt++
     }
+
+//    for (i in 1 .. num) {
+//        val lt = i
+//        var temp = 0
+//        for (j in i .. num) {
+//            temp += j
+//            if (temp > num) break
+//            else if (temp == num) {
+//                result++
+//                break
+//            }
+//        }
+//    }
 
     println(result)
 
