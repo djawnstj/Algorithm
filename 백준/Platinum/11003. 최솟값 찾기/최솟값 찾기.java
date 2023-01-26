8b6ee062219f79e24b2b7a8286dd4e1c6d287a97
djawnstj <90193598+djawnstj@users.noreply.github.com>
2023-01-26 23:05:25 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2408 ms, Memory: 618684 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -14,18 +14,25 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int L = Integer.parseInt(st.nextToken());
 
+        // Deque: 앞/뒤 양쪽으로 값을 추가하거나 삭제할 수 있음
         Deque<Node> deq = new LinkedList<>();
 
         st = new StringTokenizer(br.readLine());
+
+        // N개의 수만큼 받아서 바로 비교함으로써, 시간복잡도를 줄일 수 있음
         for (int i = 0; i < N; i++) {
             int cur = Integer.parseInt(st.nextToken());
 
+            // 최소값을 구하는 문제이므로, 현재 deque 내에서 입력받은 값보다 큰 값이 존재하면 삭제하면서 정렬 효과를 냄
             while (!deq.isEmpty() && deq.getLast().value > cur) deq.removeLast();
 
+            // deque의 최대값으로 최근 입력받은 값 추가
             deq.addLast(new Node(i, cur));
 
+            // 가장 첫번째 값이 현재 index로부터 L 이상 멀어졌으면 슬라이딩 윈도우를 벗어난것이므로 가장 첫번째 값 삭제
             if (deq.getFirst().index <= i - L) deq.removeFirst();
-            
+
+            // 가장 첫번째 값이 최소값이므로 첫번째 값을 공백과 함께 문자열에 추가
             bw.write(deq.getFirst().value + " ");
 
         }
