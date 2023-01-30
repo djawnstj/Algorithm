21b18c8b0e677902a4d3a775622d7ed9c42a897e
djawnstj <90193598+djawnstj@users.noreply.github.com>
2023-01-30 15:24:10 +0900

    [Silver I] Title: 절댓값 힙, Time: 908 ms, Memory: 44140 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.kt
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.kt
@@ -1,16 +1,18 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.*
+import kotlin.math.abs
 
 fun main() {
 
     val br = BufferedReader(InputStreamReader(System.`in`))
 
     val N = br.readLine().toInt()
+    PriorityQueue<Int>(Collections.reverseOrder())
 
     val queue: Queue<Int> = PriorityQueue() { o1, o2 ->
-        val f = Math.abs(o1)
-        val s = Math.abs(o2)
+        val f = abs(o1)
+        val s = abs(o2)
 
         return@PriorityQueue if (f == s && o1 > o2) 1 else if (f == s && o1 <= o2) -1 else f-s
     }
