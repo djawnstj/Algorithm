98e0cf1c31429c64d883c7a7966f84c55bb3d3e6
djawnstj <90193598+djawnstj@users.noreply.github.com>
2023-01-30 10:11:27 +0900

    [level 0] Title: 배열 두배 만들기, Time: 10.17 ms, Memory: 64.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(numbers: IntArray): IntArray {
+        var answer: IntArray = intArrayOf()
+        numbers.forEach { answer += (it * 2) }
+        return answer
+    }
+}
