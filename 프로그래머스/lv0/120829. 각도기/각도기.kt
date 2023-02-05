6485f7fc0487d379eed1f129eca5d2a942e3bf66
djawnstj <90193598+djawnstj@users.noreply.github.com>
2023-02-05 23:44:25 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 62.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(angle: Int): Int =
+        if (angle < 90) 1
+        else if (angle == 90) 2
+        else if (angle < 180) 3
+        else if (angle == 180) 4
+        else 0
+}
