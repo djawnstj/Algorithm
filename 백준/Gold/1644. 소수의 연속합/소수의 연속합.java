0ee2d805f019e131d0710fe75ad886b749f32a0d
djawnstj <90193598+djawnstj@users.noreply.github.com>
2023-03-09 15:12:42 +0900

    [Gold III] Title: 소수의 연속합, Time: 192 ms, Memory: 20068 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -1,47 +1,90 @@
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
+
 public class Main {
 
-   public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        
-        int n = sc.nextInt();
-       
+    static boolean[] isPrime;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
         if (n < 2) {
             System.out.println(0);
             return;
         }
-        
-        boolean[] check = new boolean[n+1];
-        for(int i=2;i<=n;i++) {
-        	for(int j=2;j<=Math.sqrt(i)+1;j++) {
-        		if(i%j==0) {
-        			check[i]=true;
-        			break;
-        		}
-        	}
+
+        isPrime = new boolean[n + 1];
+
+        setPrimeNum(n);
+
+//        List<Integer> primeNum = setPrimeNum(n);
+
+//        int answer = 0;
+//        int lp = 0;
+//        int sum = primeNum.get(lp);
+//
+//        for (int rp = 1; rp < primeNum.size() - 1; rp++) {
+//            sum += primeNum.get(rp);
+//
+//            if (sum > n) {
+//                while (lp < rp && sum > n) {
+//                    sum -= primeNum.get(lp++);
+//                }
+//            }
+//
+//            if (sum == n) {
+//                answer++;
+//            }
+//
+//        }
+//
+//        if (isPrime[n]) answer++;
+
+        int answer = 0;
+        int rp = 0;
+        int sum = 0;
+
+        for (int lp = 0; lp <= n; lp++) {
+            while (sum < n && rp < n) {
+                if (isPrime[rp]) sum += rp;
+                rp++;
+            }
+
+            if (sum == n && isPrime[lp]) answer++;
+            if (isPrime[lp]) sum -= lp;
         }
-        check[0]=check[1]=true;
-        check[2]=false;
-        
-        int s=0,e=0,count=0;
-        long sum=0;
-        for(s=0;s<=n;s++) {
-        	while(sum<n && e<=n) {
-    			if(check[e]==false) {
-    				sum+=e;
-    			}
-        		e++;
-        	}
-        	if(sum==n && check[s]==false) {
-        		count++;
-        	}
-        	if(check[s]==false)
-        		sum-=s;
-        	
+
+        if (isPrime[n]) answer++;
+//
+        System.out.println(answer);
+    }
+
+    public static void setPrimeNum(int num) {
+
+//        List<Integer> primeNum = new ArrayList<>();
+
+        for (int i = 2; i <= num; i++) {
+            isPrime[i] = true;
         }
-        System.out.println(count);
-        
-   }
-   
+
+        for (int i = 2; i <= num; i++) {
+            if (i > 1000) break;
+            else if (!isPrime[i]) continue;
+            for (int j = i * i; j <= num; j+=i) {
+                isPrime[j] = false;
+            }
+        }
+
+//        for (int i = 2; i <= num; i++) if (isPrime[i] == 0) primeNum.add(i);
+
+//        return primeNum;
+    }
+
 }
