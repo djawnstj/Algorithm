e740651f1fe0ac4f9182cf615dd5bc7c722a92a5
djawnstj <90193598+djawnstj@users.noreply.github.com>
2024-03-12 10:09:47 +0900

    [Silver IV] Title: 카드2, Time: 156 ms, Memory: 43960 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.kt
+++ 백준/Silver/2164. 카드2/카드2.kt
@@ -1,19 +1,26 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.util.*
 
 fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    var st = StringTokenizer(br.readLine())
 
-    val sc = Scanner(System.`in`)
-    val N = sc.nextInt()
+    val n = st.nextInt()
 
     val queue: Queue<Int> = LinkedList()
 
-    for (i in 1 .. N) queue.add(i)
+    for (i in 1 .. n) {
+        queue.offer(i)
+    }
 
     while (queue.size > 1) {
         queue.poll()
-        queue.add(queue.poll())
+        val poll = queue.poll()
+        queue.offer(poll)
     }
 
-    println(queue.peek())
-
+    println(queue.poll())
 }
+
+fun StringTokenizer.nextInt(): Int = nextToken().toInt()
