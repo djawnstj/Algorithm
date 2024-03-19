bd5e2c5ca5b4cb3333bde0632145490e0c7a34e8
djawnstj <90193598+djawnstj@users.noreply.github.com>
2024-03-19 09:10:40 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 18644 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.kt
+++ 백준/Silver/11047. 동전 0/동전 0.kt
@@ -6,20 +6,18 @@ fun main() {
     val st = StringTokenizer(br.readLine())
 
     val n = st.nextInt()
-    val k = st.nextInt()
+    var k = st.nextInt()
 
     val arr = IntArray(n) { StringTokenizer(br.readLine()).nextInt() }
 
-    var temp = 0
     var result = 0
 
     arr.sortedArrayDescending().forEach {
-        val target = k - temp
-        if (it > target) return@forEach
+        if (it > k) return@forEach
 
-        val mock = target / it
+        val mock = k / it
         if (mock > 0) {
-            temp += it * mock
+            k -= it * mock
             result+=mock
         }
     }
