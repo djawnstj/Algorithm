395297d41cf5e09a44b4ece23853490a9e3a4e5c
djawnstj <90193598+djawnstj@users.noreply.github.com>
2024-05-02 10:33:12 +0900

    [Gold IV] Title: 뱀, Time: 120 ms, Memory: 13448 KB -BaekjoonHub

---
+++ 백준/Gold/3190. 뱀/뱀.kt
@@ -0,0 +1,131 @@
+import Direction.Companion.rotate
+import java.io.*
+import java.util.*
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val n = br.readInt()
+
+    val k = br.readInt()
+
+    val apples = Array(n) { BooleanArray(n) }
+
+    repeat(k) {
+        val st = StringTokenizer(br.readLine())
+        val x = st.nextInt() - 1
+        val y = st.nextInt() - 1
+
+        apples[x][y] = true
+    }
+
+    val l = br.readInt()
+
+    val directionChanges: Queue<DirectionChange> = LinkedList()
+
+    repeat(l) {
+        val st = StringTokenizer(br.readLine())
+        val x = st.nextInt()
+        val c = st.nextToken()
+
+        directionChanges.add(DirectionChange(x, c))
+    }
+
+    val bam: Deque<Bam> = LinkedList()
+
+    var result = 0
+    var direction = Direction.RIGHT
+
+//    apples.joinToString(separator = "\n", transform = {
+//        it.joinToString(separator = " ", transform = { apple ->
+//            if (apple) "[x]" else "[ ]"
+//        })
+//    }).println()
+
+    bam.add(Bam(0, 0))
+
+    while (true) {
+        result++
+
+        val directionChange = directionChanges.peek()
+
+//        println("---------------------------------------")
+//        println("time: $result, direction: $direction, directionChange: $directionChange(${directionChange != null && directionChange.time == result})")
+
+        val head = bam.peekFirst()
+        val nextHead = head.next(direction)
+
+//        println("head: $head, nextHead: $nextHead")
+//        println("meetSelf: ${bam.contains(nextHead)}, meetWall: ${nextHead.checkCrush(n)}")
+        if (bam.contains(nextHead)) break
+        if (nextHead.checkCrush(n)) break
+
+        bam.addFirst(nextHead)
+
+        if (!apples[nextHead.x][nextHead.y]) bam.pollLast()
+        else apples[nextHead.x][nextHead.y] = false
+
+//        println("bam: ${bam.size}")
+//        repeat(n) { x ->
+//            val sb = StringBuilder()
+//            repeat(n) { y ->
+//                val s = if (bam.contains(Bam(x, y))) "[x]" else "[ ]"
+//                sb.append(s)
+//            }
+//            sb.toString().println()
+//        }
+//        println("---------------------------------------")
+
+        if (directionChange != null && directionChange.time == result) {
+            direction = direction.rotate(directionChange.direction)
+            directionChanges.poll()
+        }
+    }
+
+    println(result)
+
+    br.close()
+}
+
+enum class Direction(
+    val num: Int
+) {
+    UP(0), RIGHT(1), DOWN(2), LEFT(3);
+
+    companion object {
+        fun Direction.rotate(direction: String): Direction {
+            val directionTemp = if (direction == "L") num - 1
+            else num + 1
+
+            val directionNum = if (directionTemp < 0) 3
+            else if (directionTemp >= 4) 0
+            else directionTemp
+
+            return values()[directionNum]
+        }
+    }
+}
+
+data class DirectionChange(
+    val time: Int,
+    val direction: String
+)
+
+data class Bam(
+    val x: Int,
+    val y: Int
+) {
+    fun next(direction: Direction): Bam {
+        return when (direction) {
+            Direction.UP -> Bam(x - 1, y)
+            Direction.RIGHT -> Bam(x, y + 1)
+            Direction.DOWN -> Bam(x + 1, y)
+            Direction.LEFT -> Bam(x, y - 1)
+        }
+    }
+
+    fun checkCrush(n: Int): Boolean = (x < 0 || x >= n || y < 0 || y >= n)
+}
+
+private fun BufferedReader.readInt(): Int = readLine().toInt()
+private fun StringTokenizer.nextInt(): Int = nextToken().toInt()
+private fun String.println() = println(this)
